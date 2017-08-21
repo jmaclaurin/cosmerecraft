4916216e1158aaf50bc52556a35851d09a096806
Jessica Zoller <maclaurin.jess@gmail.com>
2017-08-21 07:19:42 +0200

    Added steel block.

--- src/main/java/com/mistcraft/common/block/ModBlocks.java
+++ src/main/java/com/mistcraft/common/block/ModBlocks.java
@@ -11,6 +11,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public final class ModBlocks {
     public static Block COPPER_BLOCK = new BlockBase("copper_block");
     public static Block COPPER_ORE = new BlockBase("copper_ore");
+    public static Block STEEL_BLOCK = new BlockBase("steel_block");
     public static Block TIN_BLOCK = new BlockBase("tin_block");
     public static Block TIN_ORE = new BlockBase("tin_ore");
     public static Block ZINC_BLOCK = new BlockBase("zinc_block");
@@ -21,6 +22,7 @@ public final class ModBlocks {
         event.getRegistry().registerAll(
                 COPPER_BLOCK,
                 COPPER_ORE,
+                STEEL_BLOCK,
                 TIN_BLOCK,
                 TIN_ORE,
                 ZINC_BLOCK,
@@ -33,6 +35,7 @@ public final class ModBlocks {
         event.getRegistry().registerAll(
                 new ItemBlock(COPPER_BLOCK).setRegistryName(COPPER_BLOCK.getRegistryName()),
                 new ItemBlock(COPPER_ORE).setRegistryName(COPPER_ORE.getRegistryName()),
+                new ItemBlock(STEEL_BLOCK).setRegistryName(COPPER_ORE.getRegistryName()),
                 new ItemBlock(TIN_BLOCK).setRegistryName(TIN_BLOCK.getRegistryName()),
                 new ItemBlock(TIN_ORE).setRegistryName(TIN_ORE.getRegistryName()),
                 new ItemBlock(ZINC_BLOCK).setRegistryName(ZINC_BLOCK.getRegistryName()),
