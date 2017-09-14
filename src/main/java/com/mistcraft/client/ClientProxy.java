98e4b2abe4324565426ddfdaa45e60351087558d
Jessica Zoller <maclaurin.jess@gmail.com>
2017-09-14 08:05:48 +0200

    Add colors to metal vials.

--- src/main/java/com/mistcraft/client/ClientProxy.java
+++ src/main/java/com/mistcraft/client/ClientProxy.java
@@ -1,6 +1,11 @@
 package com.mistcraft.client;
 
 import com.mistcraft.common.CommonProxy;
+import com.mistcraft.common.item.ItemVial;
+import com.mistcraft.common.item.ModItems;
+import com.mistcraft.common.vial.Vial;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -12,6 +17,16 @@ public class ClientProxy extends CommonProxy {
 
     public void init(FMLInitializationEvent event) {
         super.init(event);
+
+        // Register Item Color Handlers
+        ItemColors itemColors = Minecraft.getMinecraft().getItemColors();
+        itemColors.registerItemColorHandler((s, t) -> {
+            if(t != 1) {
+                return -1;
+            }
+            Vial vial = (((ItemVial)s.getItem()).getVial(s));
+            return vial.getMetal().color();
+        }, ModItems.VIAL);
     }
 
     public void postInit(FMLPostInitializationEvent event) {
--- src/main/java/com/mistcraft/common/util/AllomanticMetal.java
+++ src/main/java/com/mistcraft/common/util/AllomanticMetal.java
@@ -1,5 +1,24 @@
 package com.mistcraft.common.util;
 
 public enum AllomanticMetal {
-    BRASS, BRONZE, COPPER, ELECTRUM, GOLD, IRON, PEWTER, STEEL, TIN, ZINC
+    BRASS(11904578),
+    BRONZE(13467442),
+    COPPER(12088115),
+    ELECTRUM(9602857),
+    GOLD(13938487),
+    IRON(12367026),
+    PEWTER(9017993),
+    STEEL(11584734),
+    TIN(13882581),
+    ZINC(12240072);
+
+    private final int color;
+
+    AllomanticMetal(int color) {
+        this.color = color;
+    }
+
+    public int color() {
+        return this.color;
+    }
 }
